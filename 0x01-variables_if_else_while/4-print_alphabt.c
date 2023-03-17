4a2508bf812eb6ce194ace6f1989660fb80ffee9
Fabian Simiyu <fabiansimiyu177@gmail.com>
2023-03-17 12:52:39 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *
+  *Return: is 0 on success
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
