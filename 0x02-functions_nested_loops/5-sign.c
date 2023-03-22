4e577d59ce25339d79beb485516509f81fcb4589
Fabian Simiyu <fabiansimiyu177@gmail.com>
2023-03-22 15:05:59 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   *print_sign - function that prints signs
@@ -13,17 +12,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (0);
+		_putchar(43);
+		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar('-');
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
