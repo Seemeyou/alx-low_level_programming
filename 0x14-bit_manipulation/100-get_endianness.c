61e5420c245e9f5afa6dd89d73f8b4fb88b57119
simiyufabby <fabiansimiyu177@gmail.com>
2023-08-31 08:57:49 -0700

    commit

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of the system.
- *
- * Return: 0 if big endian, 1 if little endian.
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
-	unsigned int num = 1;
-	char *ch = (char *)&num;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-	return (int)(*ch);
+	return (*c);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,14 +9,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int diff = n ^ m;
-	unsigned int count = 0;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	while (diff > 0)
+	for (i = 63; i >= 0; i--)
 	{
-		count += diff & 1;
-		diff >>= 1;
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
 	return (count);
 }
+
